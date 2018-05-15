9b63cbd15a57e227501a47f86bc52111b352ed33
corinne2price <39285975+corinne2price@users.noreply.github.com>
2018-05-14 20:51:27 -0500

    Add files via upload

--- find_rep.py
+++ find_rep.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-
-oldpdb = open("sample.txt", 'r').read()
-
+#Good for psf and pdb files (up to one replacement per line)
 
 dic = {} #dictionary that will hold old and new names
 
 
-with open("dict") as f: #dict = text file with old in col1 and new in col2
+with open("names_rev.txt") as f: #fill dictionary
     for line in f:
        (old, new) = line.split()
        dic[(old)] = new
+       
+oldpdb = open("Erg1.pdb", 'r') 
+newpdb = open("replaced_Erg1.pdb", 'w')
 
-#print(dic)
-
-for i, j in dic.items():
-	print("replaced", i, "with",j)
-	oldpdb = (oldpdb.replace(i, j))
-
-
-open("replaced.txt", 'w').write(oldpdb)
+for line in oldpdb:
+	original = line
+	for i, j in dic.items():
+		if i in line:
+			if original == line and line[line.rindex(i) +1].isspace():	# make sure old hasn't already been replaced, and make sure entire key is replaced
+				line = line.replace(i,j)		#replace old with new
+	newpdb.write(line)
+	
